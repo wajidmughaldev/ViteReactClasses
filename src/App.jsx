c82e0b03166f9273f45035e38569bcafd1bf9400
wajid mughal <wajidmughalwm2001@gmail.com>
2025-07-10 23:15:21 +0500

    intermediate level state concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,35 @@
-import { useState } from "react";
-import "./App.css";
+import React from 'react'
+import{useState} from 'react'
+import Child from './components/Child'
+
+
 const App = () => {
-  const [isData, setData] = useState([
-    { id: 1, name: "Ali Raza", age: 20, grade: "A", email: "ali@example.com" },
-    {
-      id: 2,
-      name: "Sara Khan",
-      age: 22,
-      grade: "B",
-      email: "sara@example.com",
-    },
-    {
-      id: 3,
-      name: "Usman Tariq",
-      age: 19,
-      grade: "A+",
-      email: "usman@example.com",
-    },
-    {
-      id: 4,
-      name: "Hina Shah",
-      age: 21,
-      grade: "C",
-      email: "hina@example.com",
-    },
-    {
-      id: 5,
-      name: "Bilal Ahmed",
-      age: 23,
-      grade: "B+",
-      email: "bilal@example.com",
-    },
-    {
-      id: 6,
-      name: "Maria Yousuf",
-      age: 20,
-      grade: "A",
-      email: "maria@example.com",
-    },
-    {
-      id: 7,
-      name: "Zain Malik",
-      age: 22,
-      grade: "B",
-      email: "zain@example.com",
-    },
-    {
-      id: 8,
-      name: "Fatima Noor",
-      age: 18,
-      grade: "A+",
-      email: "fatima@example.com",
-    },
-    {
-      id: 9,
-      name: "Hamza Ali",
-      age: 24,
-      grade: "C+",
-      email: "hamza@example.com",
-    },
-    {
-      id: 10,
-      name: "Iqra Siddiqui",
-      age: 21,
-      grade: "B-",
-      email: "iqra@example.com",
-    },
-  ]);
-
-
-  const FindById=()=>{
-    const student = isData.find((s) => s.id === 2);
-    if (student) {
-      alert(`Found: ${student.name}, Age: ${student.age}, Email: ${student.email}`);
-    } else {
-      alert("Student not found");
-    }
+  
+  const [isTasks, setIsTasks] = useState([])
+  const [isVal, setIsVal] = useState('')
+  const formHandler = (e) => {
+  e.preventDefault()
+    setIsTasks([...isTasks,e.target[0].value])
+    // console.log(e.target[0].value)
+    e.target[0].value = ''
   }
 
-
-  return(
+ 
+  return (
     <>
-    <section className="flex flex-wrap gap-8">
-      {/* {
-        isData.map((sutdent,index)=>(
-        
-            <div key={index} className="card bg-amber-100 p-5 rounded-xl">
-              <small className="name">{sutdent.id}</small>
-              <h2 className="name">{sutdent.name}</h2>
-              <p className="age">Age: {sutdent.age}</p>
-              <p className="email">Email: {sutdent.email}</p>
-              <p className="grade">Grade: {sutdent.grade}</p>
-            </div>
-          
-        ))
-      } */}
-
-    <FindById/>
-
-    </section>
+      <form onSubmit={formHandler}>
+        <input type="text" placeholder='Call To Admin, Meeting...' value={isVal}  onChange={(e)=>setIsVal(e.target.value)} />
+        <input type="submit" value='Add' />
+        <input type="button" value='clear' onClick={()=>setIsVal('')}/>
+      </form>
+      <ul className='tasklist'>
+        {
+          isTasks.map((task)=><li>{task}</li>)
+        }
+      </ul>
+      <Child inputVal={isVal}/>
     </>
   )
-};
+}
 
-export default App;
+export default App
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-const Child = () => {
+const Child = ({inputVal}) => {
 
   return (
     <>
-    
+    <h1>Child Component</h1>
+      <h1>{inputVal}</h1>
     </>
   )
 }
